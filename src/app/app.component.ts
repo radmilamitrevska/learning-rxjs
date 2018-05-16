34ec5dc9bc6ef1c193eb03770222b47e8e17bd4d
Radmila Mitrevska <radmila.mitrevska@psp-media.com>
2018-05-16 16:07:21 +0200

    adding observable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  observable$;
+
+  ngOnInit() {
+    this.observable$ = Observable.create( (observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    this.observable$.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('this is the end')
+    );
+  }
+  ngOnDestroy() {
+    this.observable$.unsubscribe();
+  }
 }
